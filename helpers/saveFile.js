3a46794e67843afc43b949f2fa8b060f38d4eb45
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-31 22:26:21 +0200

    [TASK] create helper function to save tasks in datebase

---
+++ helpers/saveFile.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+const file = './db/data.json';
+
+const saveDB = (data) => {
+    
+    fs.writeFileSync( file, JSON.stringify(data) );
+}
+
+const readDB = () => {
+    if( !fs.existsSync(file) ) {
+        return null;
+    }
+    const info = fs.readFileSync(file, { encoding: 'utf-8'} );
+    return JSON.parse( info );
+
+}
+
+export { saveDB, readDB };
